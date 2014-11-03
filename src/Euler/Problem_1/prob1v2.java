48bcaef296c8050685b1ed09f82bccfca975846f
vchen02 <vchen02@gmail.com>
2014-11-03 15:53:36 -0800

    Prob1 Solved
    Added version 1 and version of solution for problem 1

--- src/Euler/Problem_1/prob1v2.java
+++ src/Euler/Problem_1/prob1v2.java
@@ -1,5 +1,6 @@
 package Euler.Problem_1;
 
+import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -19,13 +20,38 @@ import java.util.TreeSet;
  * 
  */
 
+class CustomSet extends TreeSet
+{
+	private int sum;
+	private Iterator itr;
+	
+	public int sum_of_set()
+	{
+		itr = iterator(); 		//grab the iterator object of the first item on the set
+		Integer intObj = null;
+		while (itr.hasNext())
+		{
+			intObj = (Integer)itr.next();
+			sum += intObj;
+		}
+		return sum;
+		
+	}
+}
 public class prob1v2 {
 	public static void main (String[] args)
 	{
 		final int MAX = 1000, N1 = 3, N2 = 5;
-		Set multiple_set_3_5 = new TreeSet();
-		int sum = 0;
+		CustomSet multiple_set_3_5 = new CustomSet();
 		
-		System.out.println("The sum of the multiples of 3 or 5 less than 1000 is: " + sum);
+		for (int i =1; i < (MAX+2)/N1; i ++ )
+		{
+			multiple_set_3_5.add(new Integer(i *N1));
+			if (i*N2 < MAX)
+			{
+				multiple_set_3_5.add(new Integer(i *N2));
+			}
+		}
+		System.out.println("The sum of the multiples of 3 or 5 less than 1000 is: " + multiple_set_3_5.sum_of_set() );
 	}
 }
